5bbd50dd2317ab5809280348f3c55171d498a5db
leibin <857247710@qq.com>
2020-08-06 21:08:56 +0800

    first commit

---
+++ src/Server.ts
@@ -0,0 +1,68 @@
+import './LoadEnv'; // Must be the first import
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
+const { stMonitor, stLogger, stHttpLoggerMiddleware } = require('sematext-agent-express')
+
+import express, { Request, Response, NextFunction } from 'express';
+import { BAD_REQUEST } from 'http-status-codes';
+import 'express-async-errors';
+
+import BaseRouter from './routes';
+import logger from '@shared/Logger';
+
+console.log(process.env.MONITORING_TOKEN)
+console.log(stMonitor)
+// Init express
+const app = express();
+
+(stMonitor as any).start()
+
+/************************************************************************************
+ *                              Set basic express settings
+ ***********************************************************************************/
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(stHttpLoggerMiddleware)
+
+
+// Show routes called in console during development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Security
+if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+}
+
+// Add APIs
+app.use('/api', BaseRouter);
+
+// Print API errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+        error: err.message,
+    });
+});
+
+
+
+/************************************************************************************
+ *                              Serve front-end content
+ ***********************************************************************************/
+
+const viewsDir = path.join(__dirname, 'views');
+app.set('views', viewsDir);
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile('index.html', {root: viewsDir});
+});
+
+// Export express instance
+export default app;
